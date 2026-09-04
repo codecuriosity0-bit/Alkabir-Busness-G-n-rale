454e55b2f4ba09adb29a9b342439d3470262b659
Code&Curiosity <codecuriosity0@gmail.com>
2026-09-04 14:40:40 +0000

    Add files via upload

---
+++ sw 18.11.58.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'abg-admin-v1';
+const ASSETS_TO_CACHE = [
+  './admin.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Réseau d'abord pour toujours avoir les données Firestore à jour,
+  // secours sur le cache si hors-ligne (utile surtout pour la coquille de l'app).
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
